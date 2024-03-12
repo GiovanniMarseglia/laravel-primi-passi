90124bdb82a77019a74fe23d2774d1d8e2ebc640
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-03-12 13:09:37 +0100

    added static h1 added dynamic h3

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+
+    $data=[
+        'saluto' =>'Hello world',
+    ];
+
+    return view('home', $data);
 });
